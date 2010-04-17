59cf162414fee7f642d2b6b54bd1f589f3cdcb54
bharath.vegito <bharath.vegito@07390328-0b26-3f57-d36a-2884105ebc08>
2010-04-17 13:13:41 +0000

    Inital Commit of the project

---
+++ trunk/BlueCabProject/src/BlueCabServiceProvider/CabProvider.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package BlueCabServiceProvider;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
+
+/**
+ * @author GAMEBOY
+ *
+ */
+public class CabProvider implements DiscoveryListener {
+
+	public CabProvider() {
+
+		try {
+			LocalDevice localDevice = LocalDevice.getLocalDevice();
+			DiscoveryAgent disAgent = localDevice.getDiscoveryAgent();
+			disAgent.startInquiry(DiscoveryAgent.GIAC, this);
+		} catch (BluetoothStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void deviceDiscovered(RemoteDevice arg0, DeviceClass arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void inquiryCompleted(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void serviceSearchCompleted(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ trunk/BlueCabProject/src/mainPackage/BlueCabManager.java
@@ -0,0 +1,20 @@
+package mainPackage;
+/**
+ * 
+ */
+
+/**
+ * @author Rohit
+ *
+ */
+public class BlueCabManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
