81ea5a7b1ea7aadabbbc5320a85c2a56764c257f
rohitalgeri <rohitalgeri@07390328-0b26-3f57-d36a-2884105ebc08>
2010-04-24 13:24:18 +0000

    First Connectivity Code for CabRequester

--- BlueCabProject/src/BlueCabRequester/CabRequester.java
+++ BlueCabProject/src/BlueCabRequester/CabRequester.java
@@ -2,6 +2,8 @@
  * 
  */
 package BlueCabRequester;
+import java.io.DataOutputStream;
+
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.DiscoveryAgent;
@@ -9,6 +11,9 @@ import javax.bluetooth.DiscoveryListener;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
 
 
 
@@ -17,6 +22,8 @@ import javax.bluetooth.ServiceRecord;
  *
  */
 public class CabRequester implements DiscoveryListener {
+	private RemoteDevice rd;
+	private ServiceRecord service;
 	
 	public CabRequester(){
 		
@@ -24,6 +31,13 @@ public class CabRequester implements DiscoveryListener {
 			LocalDevice localDevice = LocalDevice.getLocalDevice();
 			DiscoveryAgent disAgent = localDevice.getDiscoveryAgent();
 			disAgent.startInquiry(DiscoveryAgent.GIAC, this);
+			UUID uuid = new UUID(0x0003);
+			disAgent.searchServices(new int[]{0x0100},new UUID[]{uuid},rd,this);
+			if(localDevice==null)
+				System.out.println("No local Device Found");
+			else{
+				System.out.println("Local Device found"+localDevice.getFriendlyName());
+				}
 		} catch (BluetoothStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,21 +47,35 @@ public class CabRequester implements DiscoveryListener {
 
 	public void deviceDiscovered(RemoteDevice arg0, DeviceClass arg1) {
 		// TODO Auto-generated method stub
-		
+		rd=arg0;
 	}
 
 	public void inquiryCompleted(int arg0) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("All nearby devices has been detected");
 	}
 
 	public void serviceSearchCompleted(int arg0, int arg1) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("All services has been detected");
 	}
 
 	public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
 		// TODO Auto-generated method stub
-		
+		service = arg1[0];
+	}
+	public void createconnection()
+	{
+		try 
+		{
+			String url = service.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+			StreamConnection connection = (StreamConnection) Connector.open(url);
+			DataOutputStream output = connection.openDataOutputStream();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 }
