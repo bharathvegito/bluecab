e88a498d6890b9c2c5352a9cf460cf5288503ecc
rohitalgeri <rohitalgeri@07390328-0b26-3f57-d36a-2884105ebc08>
2010-04-24 13:59:34 +0000

    Committing with CabProvider Connectivity code embedded. Basic Connectivity code now ready

--- BlueCabProject/src/BlueCabServiceProvider/CabProvider.java
+++ BlueCabProject/src/BlueCabServiceProvider/CabProvider.java
@@ -3,6 +3,8 @@
  */
 package BlueCabServiceProvider;
 
+import java.io.DataInputStream;
+
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.DiscoveryAgent;
@@ -10,19 +12,25 @@ import javax.bluetooth.DiscoveryListener;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
 
 /**
  * @author GAMEBOY
  *
  */
 public class CabProvider implements DiscoveryListener {
+	
+	UUID uuid = new UUID(0x0009);
+	StreamConnectionNotifier connection; 
 
 	public CabProvider() {
 
 		try {
 			LocalDevice localDevice = LocalDevice.getLocalDevice();
-			DiscoveryAgent disAgent = localDevice.getDiscoveryAgent();
-			disAgent.startInquiry(DiscoveryAgent.GIAC, this);
+			localDevice.setDiscoverable(DiscoveryAgent.GIAC);
 		} catch (BluetoothStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,4 +57,17 @@ public class CabProvider implements DiscoveryListener {
 		// TODO Auto-generated method stub
 
 	}
+	public void acceptconnections()
+	{
+		try{
+			connection = (StreamConnectionNotifier)Connector.open("btspp://localhost:"+uuid+";name=CabProvider;authorize=false");
+			StreamConnection conn = connection.acceptAndOpen();
+			DataInputStream in = conn.openDataInputStream();
+		}
+		catch(Exception e)
+		{
+				e.printStackTrace();
+		}
+	}
+	
 }
