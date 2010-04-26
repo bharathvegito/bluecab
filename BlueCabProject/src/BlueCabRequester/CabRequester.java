d223cc32c8cd0eb3c584dea6da4f46357b9b0893
bharath.vegito <bharath.vegito@07390328-0b26-3f57-d36a-2884105ebc08>
2010-04-26 17:28:31 +0000

    Initial Test level commit

--- BlueCabProject/src/BlueCabRequester/CabRequester.java
+++ BlueCabProject/src/BlueCabRequester/CabRequester.java
@@ -1,8 +1,7 @@
-/**
- * 
- */
 package BlueCabRequester;
+
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DeviceClass;
@@ -15,67 +14,110 @@ import javax.bluetooth.UUID;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 
-
-
 /**
- * @author GAMEBOY
- *
+ * @author Rohit
+ * @description
  */
 public class CabRequester implements DiscoveryListener {
-	private RemoteDevice rd;
+	private RemoteDevice remoteDevice;
 	private ServiceRecord service;
-	
-	public CabRequester(){
-		
+
+	public CabRequester() throws BluetoothStateException {
+		LocalDevice localDevice = LocalDevice.getLocalDevice();
+		// Local device object for localhost
+		if (localDevice == null) {
+			System.out.println("No local Device Found");
+			System.exit(1);
+		}
+		System.out
+				.println("Local Device found" + localDevice.getFriendlyName());
+
+		DiscoveryAgent disAgent = localDevice.getDiscoveryAgent();
+		// object that does services search after getting a device
+		// For each device found get all its services providable by it
+		disAgent.startInquiry(DiscoveryAgent.GIAC, this);
+		// start enquiry for device(s)
+		UUID uuid = new UUID(0x0003);
+		// uuid is array for set of protocols, we need only RFCOM for
+		// passing msges
+		disAgent.searchServices(new int[] { 0x0100 }, new UUID[] { uuid },
+				remoteDevice, this);
+		// Search for the service offered by the remote device
+	}
+
+	public void deviceDiscovered(RemoteDevice rdDiscovered, DeviceClass arg1) {
 		try {
-			LocalDevice localDevice = LocalDevice.getLocalDevice();
-			DiscoveryAgent disAgent = localDevice.getDiscoveryAgent();
-			disAgent.startInquiry(DiscoveryAgent.GIAC, this);
-			UUID uuid = new UUID(0x0003);
-			disAgent.searchServices(new int[]{0x0100},new UUID[]{uuid},rd,this);
-			if(localDevice==null)
-				System.out.println("No local Device Found");
-			else{
-				System.out.println("Local Device found"+localDevice.getFriendlyName());
-				}
-		} catch (BluetoothStateException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Remote device discovered"
+					+ rdDiscovered.getFriendlyName(true));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		remoteDevice = rdDiscovered;
 	}
 
-	public void deviceDiscovered(RemoteDevice arg0, DeviceClass arg1) {
-		// TODO Auto-generated method stub
-		rd=arg0;
+	public void inquiryCompleted(int inqueryStatus) {
+		switch (inqueryStatus) {
+		case INQUIRY_COMPLETED:
+			System.out.println("All nearby devices has been detected");
+			break;
+		case INQUIRY_ERROR:
+			System.out.println("Remote Device not found");
+			System.exit(INQUIRY_ERROR);
+			break;
+		case INQUIRY_TERMINATED:
+			System.out.println("Enquiry cancelled by user");
+			System.exit(INQUIRY_TERMINATED);
+			break;
+		default:
+			if (remoteDevice == null)
+				System.out.println("Unknown Error");
+			break;
+		}
 	}
 
-	public void inquiryCompleted(int arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("All nearby devices has been detected");
-	}
-
-	public void serviceSearchCompleted(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("All services has been detected");
+	public void serviceSearchCompleted(int transID, int responseCode) {
+		switch (responseCode) {
+		case SERVICE_SEARCH_COMPLETED:
+			System.out
+					.println("All nearby servies offered by the remote device has been detected");
+			break;
+		case SERVICE_SEARCH_ERROR:
+			System.out.println("Could not get services offered");
+			System.exit(SERVICE_SEARCH_ERROR);
+			break;
+		case SERVICE_SEARCH_TERMINATED:
+			System.out.println("Service enquiry cancelled by user");
+			System.exit(SERVICE_SEARCH_TERMINATED);
+			break;
+		}
 	}
 
 	public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
-		// TODO Auto-generated method stub
+		System.out.println("Services found");
 		service = arg1[0];
+		createconnection();
 	}
-	public void createconnection()
-	{
-		try 
-		{
-			String url = service.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
-			StreamConnection connection = (StreamConnection) Connector.open(url);
+
+	public void createconnection() {
+		try {
+			String url = service.getConnectionURL(
+					ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+			// get the url from service retrieved
+			// address, port of the remote device in the url
+			StreamConnection connection = (StreamConnection) Connector
+					.open(url);
+			// Open a connection using the url
 			DataOutputStream output = connection.openDataOutputStream();
-		} 
-		catch (Exception e) 
-		{
+			output.writeUTF("Hai .....I am sending a message");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	public void customerContractSetup() {
+		// TODO: Message passing to write
+		// to check it out : Do we have to call setDiscoverable() for cab
+		// requestor
+	}
+
 }
--- BlueCabProject/src/mainPackage/BlueCabManager.java
+++ BlueCabProject/src/mainPackage/BlueCabManager.java
@@ -1,15 +1,13 @@
 package mainPackage;
 
+import javax.bluetooth.BluetoothStateException;
+
 import BlueCabRequester.CabRequester;
 import BlueCabServiceProvider.CabProvider;
 
 /**
- * 
- */
-
-/**
  * @author Rohit
- *
+ * @description
  */
 public class BlueCabManager {
 
@@ -17,10 +15,19 @@ public class BlueCabManager {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		CabRequester cr=new CabRequester();
+		CabRequester cr = null;
+		try {
+			cr = new CabRequester();
+		} catch (BluetoothStateException e) {
+			e.printStackTrace();
+		}
 		cr.createconnection();
-		CabProvider cp=new CabProvider();
+		CabProvider cp = null;
+		try {
+			cp = new CabProvider();
+		} catch (BluetoothStateException e) {
+			e.printStackTrace();
+		}
 		cp.acceptconnections();
 	}
 
